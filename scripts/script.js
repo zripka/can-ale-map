4bf42ce0ceba25ea5f49dbc69863e3c3ba0b516c
Zach Ripka <zripka@gmail.com>
2019-07-15 17:29:46 -0400

    More minor changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiZ3NkaC1ncm91cCIsImEiOiJjanh6Mzg4azkwMGMwM2RsaDIzYjd6azFnIn0.BU150ucyg0_Zhm8OCvQ89w';
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/gsdh-group/cjy09oz3k0myk1cp81d193j7l',
+    style: 'mapbox://styles/gsdh-group/cjy4sm7q502611emkkiw0ugnr',
     center: [-73.7, 45.5],
     minZoom: 8.5,
     zoom: 8.5

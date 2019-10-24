742466d2f641d154a978fb9a8ff6b9343af70a1d
Zach Ripka <zripka@gmail.com>
2019-10-24 15:23:43 -0400

    recommented js file

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,9 @@
+//script to initialize and style Can-ALE map
+
+//pass in mapbox access token
 mapboxgl.accessToken = 'pk.eyJ1IjoiZ3NkaC1ncm91cCIsImEiOiJjanh6Mzg4azkwMGMwM2RsaDIzYjd6azFnIn0.BU150ucyg0_Zhm8OCvQ89w';
+
+//create the mapboxGL map object with DOM container, map style (from mapbox account), center point, zoom limit, and default zoom level specified
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/gsdh-group/cjy4sm7q502611emkkiw0ugnr',
@@ -7,6 +12,7 @@ var map = new mapboxgl.Map({
     zoom: 8.5
 });
 
+//fade in and fade out scroll message
 $(document).ready(function(){
 		$(".scroll-message").delay(2000).fadeIn(1000).delay(1200).fadeOut(1000);
 });
